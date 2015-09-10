56add5ce0f643fe834c29e572fab3d7f777f58b5
siscof <sisco_f@hotmail.com>
2015-09-10 18:38:45 +0200

    sigo con main.py

--- main.py
+++ main.py
@@ -13,6 +13,17 @@ import gc
 import re
 import seaborn as sb
 
+def sorted_nicely( l ):
+    """ Sorts the given iterable in the way that is expected.
+
+    Required arguments:
+    l -- The iterable to be sorted.
+
+    """
+    convert = lambda text: int(text) if text.isdigit() else text
+    alphanum_key = lambda key: [convert(c) for c in re.split('([0-9]+)', key)]
+    return sorted(l, key = alphanum_key)
+
 def buscar_string(archivo, palabra):
 
     linea = None
@@ -42,7 +53,7 @@ def cargar_datos(origen_datos,archivos):
         else:
             lista_nombres.append(aux[len(aux)-1])
             
-        for bench in os.listdir(directorio_exp):
+        for bench in  sorted_nicely(os.listdir(directorio_exp)):
             if bench in BENCHMARKS :
                 args.append((bench,archivos, ["Ttunk"], directorio_exp))
 
@@ -62,39 +73,42 @@ def cargar_datos_sequencial(origen_datos,archivos):
     i = 0
     lista_nombres = []
     pool_result = []
+    
     for directorio_exp in origen_datos:
-        datos_bench = []
-        
-        #nombre la prueba
-        aux = directorio_exp.split('/')
-        if aux[len(aux)-1] == '' :
-            lista_nombres.append(aux[len(aux)-2])
-        else:
-            lista_nombres.append(aux[len(aux)-1])
-        
-        for bench in os.listdir(directorio_exp):
-            if bench in BENCHMARKS :
-                datos_bench.append(loadworker(bench,archivos, ["Ttunk"], directorio_exp))
-        
-        datos_cargados[lista_nombres[i]] = dict(zip(BENCHMARKS,datos_bench))
-        i = i + 1
+        try:
+            datos_bench = []
+            
+            #nombre la prueba
+            aux = directorio_exp.split('/')
+            if aux[len(aux)-1] == '' :
+                lista_nombres.append(aux[len(aux)-2])
+            else:
+                lista_nombres.append(aux[len(aux)-1])
+            
+            for bench in  sorted_nicely(os.listdir(directorio_exp)):
+                if bench in BENCHMARKS :
+                    datos_bench.append(loadworker(bench,archivos, ["Ttunk"], directorio_exp))
+            
+            datos_cargados[lista_nombres[i]] = dict(zip(BENCHMARKS,datos_bench))
+            i = i + 1
+        except Exception as e:
+            print('Fallo al cargar el experimento -> '+directorio_exp)
+            print(e)
 
     return datos_cargados
 
 def loadworker(bench,archivos, TESTS, directorio_exp):
     dict_general = dict()
 
-    print('cargando -> '+bench)
-    
-
     #dict_general[archivo] = contenedor()
     try:
-        pprint.pprint(directorio_exp+'/condor_log/'+bench+'.err')
+        pprint.pprint(directorio_exp+'/'+bench+'.err')
         f = open(directorio_exp+'/condor_log/'+bench+'.err')
         simend = buscar_string(f, 'SimEnd')
         f.close()
         if 'ContextsFinished' == simend :
             for archivo in archivos :
+                pprint.pprint(directorio_exp+'/'+bench+'/'+archivo)
                 dict_general[archivo] = pd.read_csv(directorio_exp+'/'+bench+'/'+archivo,sep = ',', header = 0)
 
     except Exception as e:
@@ -104,31 +118,78 @@ def loadworker(bench,archivos, TESTS, directorio_exp):
     return dict_general
     
     
-def plot_opc(ax,datos):
+def plot_opc(axis,datos,index):
     
-    intervalo = calcular_intervalo(datos[test][bench]['device-spatial-report'][''],'cycle')
+    intervalo = datos['cycle'][0]
+    datos2 = datos.set_index(datos[index].cumsum())
+    df_mean = pd.rolling_mean(datos2.loc[:,['simd_op', 'scalar_i','v_mem_op', 's_mem_i','lds_op']].sum(1)/intervalo, 20)
+    df_mean.plot(ax=axis)
     
-    for test in datos.keys():
-        for bench in datos[test].keys():
-            if intervalo == 0 :
-                intervalo = datos[test][bench]['device-spatial-report'][' cycle'][0]
-            pd.rolling_mean(datos[test][bench]['device-spatial-report'].loc[:,' total_i']/intervalo, 20)
+def plot_mshr_size(axis,datos,index):
+    
+    datos2 = datos.set_index(datos[index].cumsum())
+    df_mean = pd.rolling_mean(datos2.loc[:,['MSHR_size']], 20)
+    df_mean.plot(ax=axis)
+    
+def axis_config(ax,title,ylabel,yticks,xlabel,xticks):
+    axis.set_title(title)
+    plt.legend()
+    axis.set_ylabel(ylabel)
+    axis.set_ylim(bottom = 0)
+    axis.set_xticks([])
+    
+    
+def comprobar_estructura_datos(datos):
+    for key in  list(datos):
+        if type(datos[key]) is dict:
+            dict_vacio = comprobar_estructura_datos(datos[key])
+            if dict_vacio :
+                datos.pop(key, None)
+            
+    if bool(datos) :
+        return False
+    else:
+        return True
+            
+    
+    
             
 
 if __name__ == '__main__':
 
     BENCHMARKS = ['BinarySearch','BinomialOption','BlackScholes','DCT','DwtHaar1D','FastWalshTransform','FloydWarshall','MatrixMultiplication','MatrixTranspose','MersenneTwister','QuasiRandomSequence','RadixSort','RecursiveGaussian','Reduction','ScanLargeArrays','SimpleConvolution']
     test = "tunk"
+    bench = 'tunk'
     
     pprint.pprint("hola")
     
-    datos = cargar_datos_sequencial(["/nfs/gap/fracanma/benchmark/resultados/09-07_nmoesi_mshr32_tunk_conL1/"],["device-spatial-report","extra-report_ipc"])
+    dir_experimentos = ["/nfs/gap/fracanma/benchmark/resultados/09-09_nmoesi_mshr32_tunk_conL1/",
+    "/nfs/gap/fracanma/benchmark/resultados/09-09_nmoesi_mshr16_tunk_conL1/",
+    "/nfs/gap/fracanma/benchmark/resultados/09-09_nmoesi_mshr256_tunk_conL1/"]
+    
+    datos = cargar_datos_sequencial(dir_experimentos,["device-spatial-report","extra-report_ipc"])
+    
+    comprobar_estructura_datos(datos)
 
-    f, t = plt.subplots()
+    f, t = plt.subplots(2)
     f.set_size_inches(15, 10)
     f.set_dpi(300)
     
-    
+    for bench in BENCHMARKS:
+        for test in datos.keys():
+            try:
+                plot_opc(t[0],datos[test][bench]['device-spatial-report'], index='cycle')
+            except KeyError as e:
+                print('WARNING: KeyError in datos['+test+']['+bench+'][device-spatial-report]')
+                
+            try:
+                plot_mshr_size(t[1],datos[test][bench]['device-spatial-report'], index='cycle')
+            except KeyError as e:
+                print('WARNING: KeyError in datos['+test+']['+bench+'][device-spatial-report]')   
+                
+        f.savefig('/home/sisco/workspace/pruebas/'+bench+'_'+'opc.eps',format='eps')
+        for axis in t:
+            axis.cla()
     
     directorio_salida = "/nfs/gap/fracanma/benchmark/resultados/09-07_nmoesi_mshr32_tunk_conL1/"
     
